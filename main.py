fc286cfe3f7a0b34c6db3099bacdfabaa2db25c5
Selahaddin Inan <99096237+Han-Selo@users.noreply.github.com>
2023-01-21 16:59:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+### Web VPython 3.2 ###
+
+earth = sphere(pos=vector(0, 0, 0), radius=40000, texture=textures.earth)
+
+moon = sphere(pos=vector(0, 0, 150000),radius=10000, texture=textures.rough)
+
